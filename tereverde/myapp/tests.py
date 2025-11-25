01da2e24d83697b9bef495b93ac210a724a5f738
Tiago <tiagocondack@gmail.com>
2025-11-25 02:56:26 -0300

    Add fixtures for Trilhas and Eventos models

--- tereverde/myapp/tests.py
+++ tereverde/myapp/tests.py
@@ -1,5 +1,5 @@
 import pytest
-from app.models import Parque
+from app.models import Parque, Trilhas, Eventos
 
 @pytest.fixture
 def parque():
@@ -10,4 +10,22 @@ def parque():
         taxa_entrada=0,01,
         horario_funcionamento="08:00 - 14:00",
         dia_semana="sexta-feira",
+    )
+
+@pytest.fixture
+def trilhas ()
+    return Trilhas.objects-create(
+    nome="Trilha do Sino",
+    descricao ="Uma Trilha sensacional, com muitas belezas e uma longa caminhada",
+    dificuldade="Dificil",
+    distancia="9,55",
+    )
+
+@pytest.fixture
+def Eventos()
+    return Eventos.objects.create(
+    nome="Caminhada pelo parque",
+    descricao="Caminhada pelo Parque municipal Montanhas de Teresópolis",
+    conteudo="Venha desfrutar da mais bela caminhada da região",
+    data_publicacao="15-10-2025",
     )
