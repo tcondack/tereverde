07788df54f6c5976eece958573b1f6fea6b86a44
Tiago <tiagocondack@gmail.com>
2025-12-01 16:54:38 -0300

    update tests and requirements

--- tereverde/myapp/tests/gerais_test.py
+++ tereverde/myapp/tests/gerais_test.py
@@ -1,31 +1,14 @@
-import pytest
-from myapp.models import Parque, Trilhas, Eventos
+from django.test import TestCase
+from myapp.models import Parque, Trilhas, Eventos, Novidades
 
-@pytest.fixture
-def parque(db):
-    return Parque.objects.create(
-        nome="Parque Central",
-        descricao="Um parque muito bonito",
-        localizacao="Vieira",
-        taxa_entrada="0,01",
-        horario_funcionamento="08:00 - 14:00",
-        dia_semana="sexta-feira",
-    )
+class ParqueModelTest(TestCase):
+    def setUp(self):
+        self.parque = Parque.objects.create(
+            nome="Parque Teste",
+            descricao="Descrição do parque teste",  
+            Localuizacao="Localização do parque teste",
+            horario_funcionamento="08:00 - 18:00",
+            ativo=True,
 
-@pytest.fixture
-def trilhas (db):
-    return Trilhas.objects.create(
-    nome="Trilha do Sino",
-    descricao ="Uma Trilha sensacional, com muitas belezas e uma longa caminhada",
-    dificuldade="Dificil",
-    distancia="9,55",
-    )
+        )
 
-@pytest.fixture
-def eventos(db):
-    return Eventos.objects.create(
-    nome="Caminhada pelo parque",
-    descricao="Caminhada pelo Parque municipal Montanhas de Teresópolis",
-    conteudo="Venha desfrutar da mais bela caminhada da região",
-    data_publicacao="15-10-2025",
-    )
